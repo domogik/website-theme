a996f43d7fd8b2f390df01f335bb0805208de0d2
Domogik <marc@mirelsol.org>
2013-03-30 16:17:27 +0100

    Fix edit link

--- single.php
+++ single.php
@@ -7,9 +7,9 @@
             <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
             
             <p class="postmetadata">
-            <?php the_time('j F Y') ?><?php echo __('by', $MY_TEXTDOMAIN); ?><?php the_author() ?> | 
+            <?php the_time('j F Y') ?>&nbsp;<?php _e('by', $MY_TEXTDOMAIN); ?>&nbsp;<?php the_author() ?> | 
             <?php echo __('Category', $MY_TEXTDOMAIN); ?>:&nbsp;<?php the_category(', ') ?> | 
-            <?php echo __('Edit', $MY_TEXTDOMAIN); ?>
+            <?php edit_post_link(__('Edit', $MY_TEXTDOMAIN), ' &#124; ', ''); ?>
             </p>
 
             <div class="post_content">
