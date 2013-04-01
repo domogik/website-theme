8d971748861ea2dd30bcb7bf7df8d51536d3d0c9
Root <root@domogik.org>
2013-04-01 23:30:26 +0200

    Cosmetics

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,6 @@
     <ul>
         <?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
     </ul>
-    <!-- <?php wp_list_pages('title_li=<h2>Pages</h2>'); ?>insertion des autres pages du blog -->
     <h2>Archives</h2>
     <ul>
         <?php wp_get_archives('type=monthly'); ?>
