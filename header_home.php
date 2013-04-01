6ac7807943478f889185457600594d4658b23004
Domogik <marc@mirelsol.org>
2013-04-01 11:14:07 +0200

    Cosmetics

--- header_home.php
+++ header_home.php
@@ -1,11 +1,10 @@
 <?php include('_header_common.php') ?>
 
 <section id="banner" role="banner">
-        <div class="wrapper">
-            <?php echo __('Domogik is a modular open source home automation solution based on xPL.', $MY_TEXTDOMAIN); ?>
-        </div>
-    </section> 
-   
-    
+    <div class="wrapper">
+        <?php echo __('Domogik is a modular open source home automation solution based on xPL.', $MY_TEXTDOMAIN); ?>
+    </div>
+</section> 
+
 <div id="subbanner"></div>
 
