6461d9514c26e62a1550ced872155cbcb98fb138
Root <root@domogik.org>
2013-04-01 23:40:04 +0200

    Add i18n

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,10 @@
+<?php global $MY_TEXTDOMAIN; ?>
 <div class="col" id='sidebar'>
-    <h2>Categories</h2>
+    <h2><?php echo(__("Categories", $MY_TEXTDOMAIN)); ?></h2>
     <ul>
         <?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
     </ul>
-    <h2>Archives</h2>
+    <h2><?php echo(__("Archives", $MY_TEXTDOMAIN)); ?></h2>
     <ul>
         <?php wp_get_archives('type=monthly'); ?>
     </ul>
