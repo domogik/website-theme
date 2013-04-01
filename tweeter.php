02671cf2396e9a1a986814a66498cb1c2db4f8ed
Domogik <marc@mirelsol.org>
2013-04-01 18:35:05 +0200

    Fix twitter call

--- tweeter.php
+++ tweeter.php
@@ -3,21 +3,28 @@ Don't forget to update the code with your username on line 3. Max items to displ
 
 <?php
 include_once(ABSPATH . WPINC . '/feed.php');
-$rss = fetch_feed('https://api.twitter.com/1/statuses/#domogik.rss?screen_name=catswhocode');
-$maxitems = $rss->get_item_quantity(3);
+$rss = fetch_feed('https://api.twitter.com/1/statuses/user_timeline.rss?screen_name=domogik');
+$maxitems = $rss->get_item_quantity(5);
 $rss_items = $rss->get_items(0, $maxitems);
 ?>
 
 <ul>
-<?php if ($maxitems == 0) echo '<li>No items.</li>';
-else
-// Loop through each feed item and display each item as a hyperlink.
-foreach ( $rss_items as $item ) : ?>
-<li>
-<a href='<?php echo $item->get_permalink(); ?>'>
-<?php echo $item->get_title(); ?>
-</a>
-</li>
-<?php endforeach; ?>
+    <?php 
+    if ($maxitems == 0) {
+        echo("<li>".__("No tweet.")."</li>");
+    }
+    else
+    // Loop through each feed item and display each item as a hyperlink.
+    foreach ($rss_items as $item) :
+    ?>
+        <li>
+            <a href='<?php echo $item->get_permalink(); ?>'>
+            <?php echo $item->get_title(); ?>
+            </a>
+        </li>
+    <?php
+
+    endforeach; 
+    ?>
 </ul>
 
