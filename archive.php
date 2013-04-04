01a8d1bf9967c0495591d6145c842ad1fedaa234
Root <root@domogik.org>
2013-04-04 16:48:01 +0200

    New design for archive.php

--- archive.php
+++ archive.php
@@ -1,24 +1,37 @@
-<?php get_header(); ?> <!-- ouvrir header.php -->
+<?php include('header.php'); ?>
+
+<div id='content' role='main'>
+    <div id='post'>
+        <?php while(have_posts()) : the_post(); ?>
+            <h2>
+                <a href="<?php the_permalink() ?>" rel="bookmark" title="Link to <?php the_title_attribute(); ?>">
+                    <?php the_title(); ?>
+                </a>
+            </h2>
+                    
+            <p class="postmetadata">
+            <?php the_time('j F Y') ?>&nbsp;<?php _e('by', $MY_TEXTDOMAIN); ?>&nbsp;<?php the_author() ?> | 
+            <?php echo __('Category', $MY_TEXTDOMAIN); ?>:&nbsp;<?php the_category(', ') ?> | 
+            <?php edit_post_link(__('Edit', $MY_TEXTDOMAIN), ' &#124; ', ''); ?>
+            </p>                
+                    
+            <div class="post_content">
+                <?php the_content(); ?>
+                <div class="comments-template">
+                    <!-- exclude comments for News category -->
+                    <?php if (!in_category('news-'.pll_current_language())) comments_template(); ?>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    </div>
+
+    <?php get_sidebar(); ?>
+
+    <div style="clear: both"></div>
 </div>
-<div id="content"><!-- boite du content -->
-<div class="post" id="post-<?php the_ID(); ?>"><!-- appelle l'article ayant tel id -->
-<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
-<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-<p class="postmetadata"><!-- insertion date, auteur, categorie -->
- 
-		<?php the_time('j F Y') ?> par <?php the_author() ?> | 
-		Cat&eacute;gorie: <?php the_category(', ') ?> | 
-		<?php comments_popup_link('Pas de commentaires', '1 Commentaire', '% Commentaires'); ?> <?php edit_post_link('Editer', ' &#124; ', ''); ?>
- <!--  -->
-</p>
-<div class="post_content">
-<?php the_excerpt(); ?><!-- affiche que les premieres lignes du contenu -->
-</div>
-</div>
-<?php endwhile; ?>
-<?php endif; ?>
-</div>
-<?php get_sidebar(); ?><!-- appelle la sidebar -->
-<?php get_footer(); ?><!-- appelle le footer -->
+
+<?php include('footer.php'); ?>
+
 </body>
 </html>
+
