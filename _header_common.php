c9079aa0ffdbe6623e967383faf5946f22d82400
Root <root@domogik.org>
2013-04-05 10:46:24 +0200

    Cosmetics

--- _header_common.php
+++ _header_common.php
@@ -40,12 +40,11 @@
     </nav>
     <div id="searchform">
         <form method="get" action="<?php bloginfo('home'); ?>/">
-	    <div>
-		   <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
-		   <input type="submit" id="searchsubmit" value="<?php _e("Search", $MY_TEXTDOMAIN); ?>" />
-	    </div>
+        <div>
+            <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
+            <input type="submit" id="searchsubmit" value="<?php _e("Search", $MY_TEXTDOMAIN); ?>" />
+        </div>
         </form>
     </div>
 </div>
 </header>
-  
