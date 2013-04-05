f9cd110fe2370596135e8caab2ec0b106d552f0b
Root <root@domogik.org>
2013-04-05 15:15:51 +0200

    Fix display

--- page.php
+++ page.php
@@ -1,19 +1,17 @@
-<?php include('_header_common.php'); ?> <!-- ouvrir header.php -->
-<div id="subbanner"></div>
+<?php get_header(); ?>
 <div class='wrapper'>
-    <main id="content" role='main'><!-- boite du content -->
-        <article class="post" id="post-<?php the_ID(); ?>"><!-- appelle l'article ayant tel id -->
-            <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
+    <main id="content" role='main'>
+        <?php if(have_posts()) : the_post(); ?>
+        <article class="post" id="post-<?php the_ID(); ?>">
             <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
             <div class="post_content">
                 <?php the_content(); ?>
             </div>
         </article>
-        <?php endwhile; ?>
-        <?php edit_post_link('Modifier cette page', '<p>', '</p>'); ?>
+        <?php edit_post_link(__('Edit', $MY_TEXTDOMAIN), ' &#124; ', ''); ?>
         <?php endif; ?>
     </main>
 </div>
-<?php get_footer(); ?><!-- appelle le footer -->
+<?php get_footer(); ?>
 </body>
 </html>
