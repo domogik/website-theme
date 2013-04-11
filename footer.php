10e54e7949640ab1d626bb02cf29e68e5f902d87
Root <root@domogik.org>
2013-04-11 17:13:59 +0200

    Footer improvement

--- footer.php
+++ footer.php
@@ -1,13 +1,11 @@
 <footer id="footer" role="contentinfo">
     <div class='wrapper'>
         <div class="col col1">
-            <?php echo __('General mailing-list', $MY_TEXTDOMAIN); ?>:<br />
-            <div class="mailing marginb"><a href="mailto:domogik-general@lists.labs.libre-entreprise.org">domogik-general@lists.labs.libre-entreprise.org</a></div>
-            <?php echo __('Developers mailing-list', $MY_TEXTDOMAIN); ?>:<br />
-            <div class="mailing marginb"><a href="mailto:domogik-developers@lists.labs.libre-entreprise.org">domogik-developers@lists.labs.libre-entreprise.org</a></div>
+            <div class="mailing marginb"><a href="mailto:domogik-general@lists.labs.libre-entreprise.org"><?php echo __('General mailing-list', $MY_TEXTDOMAIN); ?></a></div>
+            <div class="mailing marginb"><a href="mailto:domogik-developers@lists.labs.libre-entreprise.org"><?php echo __('Developers mailing-list', $MY_TEXTDOMAIN); ?></a></div>
         </div>
         <div class="col col2">
-            <div class="irc marginb">domogik@irc.freenode.net</div>
+            <div class="irc marginb">irc.freenode.net#domogik</div>
             <div class="twitter mailing"><a href="https://twitter.com/Domogik">@domogik</a></div>
         </div>
         <div class="col col3">
