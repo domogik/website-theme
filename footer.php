3c192e8203032e6b15bc005b47ef792f2de69cfa
Root <root@domogik.org>
2013-04-11 17:25:14 +0200

    Encode email addresses

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 <footer id="footer" role="contentinfo">
     <div class='wrapper'>
         <div class="col col1">
-            <div class="mailing marginb"><a href="mailto:domogik-general@lists.labs.libre-entreprise.org"><?php echo __('General mailing-list', $MY_TEXTDOMAIN); ?></a></div>
-            <div class="mailing marginb"><a href="mailto:domogik-developers@lists.labs.libre-entreprise.org"><?php echo __('Developers mailing-list', $MY_TEXTDOMAIN); ?></a></div>
+            <div class="mailing marginb"><a href="<?php echo eae_encode_str('mailto:domogik-general@lists.labs.libre-entreprise.org'); ?>"><?php echo __('General mailing-list', $MY_TEXTDOMAIN); ?></a></div>
+            <div class="mailing marginb"><a href="<?php echo eae_encode_str('mailto:domogik-developers@lists.labs.libre-entreprise.org'); ?>"><?php echo __('Developers mailing-list', $MY_TEXTDOMAIN); ?></a></div>
         </div>
         <div class="col col2">
             <div class="irc marginb">irc.freenode.net#domogik</div>
             <div class="twitter mailing"><a href="https://twitter.com/Domogik">@domogik</a></div>
         </div>
         <div class="col col3">
-            Domogik&copy;2013
+            Domogik&copy;<?php echo date("Y"); ?>
         </div>
     </div>
 </footer>
