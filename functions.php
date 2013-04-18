9c69bae6df1ef06e473bc2ea985b375b956514e4
Root <root@domogik.org>
2013-04-18 21:34:16 +0200

    Remove extra spaces

--- functions.php
+++ functions.php
@@ -1,15 +1,12 @@
-<?php $MY_TEXTDOMAIN = 'domogik'; ?>
+<?php 
+$MY_TEXTDOMAIN = 'domogik'; 
+ini_set('display_errors', 'On');
 
-<?php ini_set('display_errors', 'On'); ?>
-
-<?php
 function custom_excerpt_length( $length ) {
 	return 20;
 }
-add_filter( 'excerpt_length', 'custom_excerpt_length', 999 )
-?>
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
-<?php
 // For theme i18n
 add_action('after_setup_theme', 'setup');
 
@@ -17,7 +14,3 @@ function setup() {
     load_theme_textdomain(wp_get_theme()->get('TextDomain'), get_template_directory() . '/languages');
 }
 ?>
-
-
-    
-
